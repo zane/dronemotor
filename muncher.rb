a74736959432f9752beaafc0f1c9615b3e8de7da
Zane Shelby <zaneshelby@gmail.com>
2013-10-03 01:43:46 -0700

    Shorter muncher implementation

--- muncher.rb
+++ muncher.rb
@@ -1,45 +1,20 @@
-require 'fileutils'
 require 'json'
 
+extensions = ["mp3", "wav", "ogg"]
+source = File.join(Dir.pwd, 'sounds')
 
-source           = Dir.pwd + '/sounds/'
-filenames        = Hash.new { |hash,key| hash[key] = Array.new }
-output           = {}
-output['sounds'] = []
-
-
-class Sound
-    def initialize(name, exts)
-        exts.each do | ext |
-            if ext.downcase == 'mp3'
-                @mp3 = name + '.' + ext
-            elsif ext.downcase == 'ogg'
-                @ogg = name + '.' + ext
-            elsif ext.downcase == 'wav'
-                @wav = name + '.' + ext
-            end
-        end
-    end
-    def to_json()
-        object = {'mp3' => @mp3, 'ogg' => @ogg, 'wav' => @wav}.to_json
-        object = JSON.parse(object)
-    end
+def extension(path)
+  File.extname(path).gsub('.', '')
 end
 
-
-Dir.foreach(source) do |filename|
-    if filename != '.' and filename != '..' and (filename.downcase.match(/(mp3|wav|ogg)/))
-        name = File.basename(filename, '.*')
-        ext = File.extname(filename).gsub('.','')
-        filenames[name] << ext
-    end
+groups = Dir.foreach(source).group_by do |path|
+  File.basename(path, '.*')
 end
 
-filenames.each { |name, exts|
-    sound = Sound.new(name,exts).to_json
-    output['sounds'].push(sound)
-}
-
-File.open('sounds.json', 'w') do |json|
-    json.write(JSON.pretty_generate(output))
+hash = groups.map do |f, paths|
+  Hash[extensions.map { |base| [base, paths.find { |path| extension(path) == x }] }]
 end
+
+json = JSON.pretty_generate({ 'sounds' => hash }.to_json)
+
+File.open('sounds.json', 'w') { |file| file.write(json) }
